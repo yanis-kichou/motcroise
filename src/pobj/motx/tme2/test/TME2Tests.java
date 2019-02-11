1a7522360073737348418ca0681fbffc02f0bd1e
Yanis Kichou <Yanis Kichou@DESKTOP-INUM0I9>
2019-02-11 11:40:48 +0100

    Fin-TMe2

--- src/pobj/motx/tme2/test/TME2Tests.java
+++ src/pobj/motx/tme2/test/TME2Tests.java
@@ -8,5 +8,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ DictionnaireTest.class, DictionnaireTest2.class, GrillePotentielTest.class, GrillePotentielTest2.class,
 		GrillePotentielTest3.class, GrillePotentielTest4.class, GrillePotentielTest5.class, GrillePotentielTest6.class })
 public class TME2Tests {
+	/*
+	 *Fin TMe2
+	 */
 
 }
